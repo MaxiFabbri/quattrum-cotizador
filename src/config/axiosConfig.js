6634888f5437ef23de49f3b3d8fe160c9a4a9a8e
Maxi Fabbri <maxifabbri1972@gmail.com>
2025-05-14 16:34:52 -0300

    baseURL from context variables

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
+
 const apiClient = axios.create({
+    baseURL: apiUrl,
     // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://backend-cotizador-quattrum.onrender.com/api',
+    // baseURL: 'https://backend-cotizador-quattrum.onrender.com/api',
     withCredentials: true, // Si necesitas manejar cookies
 });
 const apiDolar = axios.create({
