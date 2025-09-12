bddb3679367e3febf845af28d136d6f9ceb95b3c
Maxi Fabbri <maxifabbri1972@gmail.com>
2025-09-12 13:09:03 -0300

    done change limit of quotation by page

--- src/components/QuotationsContainer/QuotationContainer.jsx
+++ src/components/QuotationsContainer/QuotationContainer.jsx
@@ -24,7 +24,7 @@ const Quotations = () => {
     // Función para realizar la solicitud GET
     const fetchQuotations = async () => {
         try {
-            const response = await apiClient.get(`/quotations/paginated-new?page=${page}&name=${filter}&status=${statusFilter}&limit=25`);
+            const response = await apiClient.get(`/quotations/paginated-new?page=${page}&name=${filter}&status=${statusFilter}`);
             setQuotations(response.data.response);
         } catch (error) {
             setError("Error al cargar las cotizaciones");
