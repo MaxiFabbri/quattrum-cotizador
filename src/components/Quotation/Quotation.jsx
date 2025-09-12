1d3a220d9b4f227ab189758b6af00f4ea2dc231e
Maxi Fabbri <maxifabbri1972@gmail.com>
2025-09-12 12:59:35 -0300

    done simpler quotations container page

--- src/components/Quotation/Quotation.jsx
+++ src/components/Quotation/Quotation.jsx
@@ -8,24 +8,7 @@ import "./Quotation.css";
 const Quotation = ({ quote, onDelete }) => {
     const navigate = useNavigate(); // Hook para la navegación
     const { setIsSaved } = useContext(QuotationContext);
-    const [isProductsLoaded, setIsProductsLoaded] = useState(false); 
-    const [products, setProducts] = useState([]); // Estado para los productos
 
-    // Función para obtener productos
-    const getProducts = async (quoteId) => {
-        try {
-            const response = await apiClient.get(`/products/${quoteId}`);
-            setProducts(response.data.response); // Actualiza los productos
-            setIsProductsLoaded(true); // Cambia el estado a true una vez cargados
-        } catch (error) {
-            console.error("Error obteniendo productos:", error);
-        }
-    };
-
-    // Ejecutar `getProducts()` cuando el componente se monta
-    useEffect(() => {
-        getProducts(quote._id);
-    }, [])
 
     // Manejo de clic en la fila
     const handleRowClick = () => {
@@ -34,10 +17,11 @@ const Quotation = ({ quote, onDelete }) => {
     };
 
     // Formatear la fecha
-    const formatDate = (utcDate) => {;
+    const formatDate = (utcDate) => {
+        ;
         const date = new Date(utcDate);
         const year = date.getFullYear();
-        const month = String(date.getUTCMonth()+1).padStart(2, "0"); // Asegura 2 dígitos
+        const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // Asegura 2 dígitos
         const day = String(date.getUTCDate()).padStart(2, "0"); // Asegura 2 dígitos
         return `${day}-${month}-${year}`;
     };
@@ -55,25 +39,21 @@ const Quotation = ({ quote, onDelete }) => {
                 />
             </td>
             <td>{formatDate(quote.date)}</td>
-            <td>{quote.customerId.name}</td>
+            <td>{quote.customer.name}</td>
             <td>{quote.currency}</td>
-            <td>{quote.isKit ? "Sí" : "No"}</td>    
+            <td>{quote.isKit ? "Sí" : "No"}</td>
             <td colSpan="3" style={{ padding: "0px" }}>
-                {!isProductsLoaded ? (
-                    <p>Loading...</p>
-                ) : (
-                    <table className="products-table">
-                        <tbody>
-                            {products.map((product) => (
-                                <tr key={product._id}>
-                                    <td style={{ textAlign: "right", paddingRight: "2%"}}>{product.quantity}</td>
-                                    <td>{product.productDescription || "Prod"}</td>
-                                    <td style={{ textAlign: "right", paddingRight: "2%"}}>$ {(product.unitSellingPrice * quote.exchangeRate).toFixed(0)}.00 </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )}
+                <table className="products-table">
+                    <tbody>
+                        {quote.products.map((product) => (
+                            <tr key={product._id}>
+                                <td style={{ textAlign: "right", paddingRight: "2%" }}>{product.quantity}</td>
+                                <td>{product.productDescription || "Prod"}</td>
+                                <td style={{ textAlign: "right", paddingRight: "2%" }}>$ {(product.unitSellingPrice * quote.exchangeRate).toFixed(0)}.00 </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </td>
             <td>{quote.quoteStatus}</td>
         </tr>
