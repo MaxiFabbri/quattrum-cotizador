34915efb39b93cef225252017301aa9b53d81a28
Maxi Fabbri <maxifabbri1972@gmail.com>
2026-03-13 19:22:57 -0300

    remove-websocketsServer

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -16,18 +16,18 @@ export const AuthProvider = ({ children }) => {
 
     const socketRef = useRef(null);
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            console.log('Conectando al servidor de WebSocket...');
-            socketRef.current = io(wsUrl, {
-                auth: {
-                    userName,
-                    userRole,
-                    userId
-                },
-            });
-        }
-    }, [isAuthenticated]);
+    // useEffect(() => {
+    //     if (isAuthenticated) {
+    //         console.log('Conectando al servidor de WebSocket...');
+    //         socketRef.current = io(wsUrl, {
+    //             auth: {
+    //                 userName,
+    //                 userRole,
+    //                 userId
+    //             },
+    //         });
+    //     }
+    // }, [isAuthenticated]);
 
     const login = async (email, password) => {
         try {
